0670aee6bb5bd0d64f87039f5cf68701310f6460
Suwarno <suwarno.ong@hotmail.com>
2019-08-15 09:33:29 +0800

    chore: upgrade to react native 0.60.5 with minimum changes till it is runnable

--- android/app/src/main/java/sg/tutella/bus/MainApplication.java
+++ android/app/src/main/java/sg/tutella/bus/MainApplication.java
@@ -1,7 +1,11 @@
 package sg.tutella.bus;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
@@ -13,10 +17,8 @@ import com.RNFetchBlob.RNFetchBlobPackage;
 import com.mapbox.rctmgl.RCTMGLPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -29,16 +31,17 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new NetInfoPackage(),
-            new LottiePackage(),
-            new RCTMGLPackage(),
-            new RNFusedLocationPackage(),
-            new SvgPackage(),
-            new RNGestureHandlerPackage(),
-            new RNFetchBlobPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new NetInfoPackage());
+      // packages.add(new LottiePackage());
+      // packages.add(new RCTMGLPackage());
+      // packages.add(new RNFusedLocationPackage());
+      // packages.add(new SvgPackage());
+      // packages.add(new RNGestureHandlerPackage());
+      packages.add(new RNFetchBlobPackage());
+      return packages;
     }
 
     @Override
